8be8bf41fa700f2ec504affb1d9411aad7ab9be7
xinjian <715054307@qq.com>
2020-03-16 11:08:43 +0800

    添加大数据功能

--- TUCHX1621UNLOADUI/MainWindow.xaml.cs
+++ TUCHX1621UNLOADUI/MainWindow.xaml.cs
@@ -16,6 +16,9 @@ using BingLibrary.hjb.file;
 using TUCHX1621UNLOADUI.Model;
 using System.Diagnostics;
 using System.Data;
+using SXJLibrary;
+using OfficeOpenXml;
+using System.IO;
 
 namespace TUCHX1621UNLOADUI
 {
@@ -27,8 +30,13 @@ namespace TUCHX1621UNLOADUI
         #region 变量
         Fx5u Fx5u, Fx5u_2;
         string iniParameterPath = System.Environment.CurrentDirectory + "\\Parameter.ini";
+        string alarmExcelPath = System.Environment.CurrentDirectory + "\\X1621串线下料机报警.xlsx";
         Scan ScanA, ScanB;
-        long SWms = 0;
+        long SWms = 0;int LampColor = 1;bool[] M300; Stopwatch LampGreenSw = new Stopwatch();
+        List<AlarmData> AlarmList = new List<AlarmData>(); string CurrentAlarm = "";
+        string _PM, _GROUP1, _TRACK, _MACID, _LIGHT_ID;
+        int LampGreenElapse, LampGreenFlickerElapse, LampYellowElapse, LampYellowFlickerElapse, LampRedElapse;
+        string LastBanci;
         #endregion
         public MainWindow()
         {
@@ -50,6 +58,58 @@ namespace TUCHX1621UNLOADUI
         }
         void Init()
         {
+            PM.Text = _PM = Inifile.INIGetStringValue(iniParameterPath, "BigData", "PM", "X1621");
+            GROUP1.Text = _GROUP1 = Inifile.INIGetStringValue(iniParameterPath, "BigData", "GROUP1", "NA");
+            TRACK.Text = _TRACK = Inifile.INIGetStringValue(iniParameterPath, "BigData", "TRACK", "0102");
+            MACID.Text = _MACID = Inifile.INIGetStringValue(iniParameterPath, "BigData", "MACID", "007");
+            LIGHT_ID.Text = _LIGHT_ID = Inifile.INIGetStringValue(iniParameterPath, "BigData", "LIGHT_ID", "007");
+
+            LampGreenElapse = int.Parse(Inifile.INIGetStringValue(iniParameterPath, "BigData", "LampGreenElapse", "0"));
+            LampGreenFlickerElapse = int.Parse(Inifile.INIGetStringValue(iniParameterPath, "BigData", "LampGreenFlickerElapse", "0"));
+            LampYellowElapse = int.Parse(Inifile.INIGetStringValue(iniParameterPath, "BigData", "LampYellowElapse", "0"));
+            LampYellowFlickerElapse = int.Parse(Inifile.INIGetStringValue(iniParameterPath, "BigData", "LampYellowFlickerElapse", "0"));
+            LampRedElapse = int.Parse(Inifile.INIGetStringValue(iniParameterPath, "BigData", "LampRedElapse", "0"));
+
+            LastBanci = Inifile.INIGetStringValue(iniParameterPath, "Summary", "LastBanci", "null");
+
+            #region 报警文档
+            try
+            {
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                if (File.Exists(alarmExcelPath))
+                {
+
+                    FileInfo existingFile = new FileInfo(alarmExcelPath);
+                    using (ExcelPackage package = new ExcelPackage(existingFile))
+                    {
+                        // get the first worksheet in the workbook
+                        ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                        for (int i = 1; i <= worksheet.Dimension.End.Row; i++)
+                        {
+                            AlarmData ad = new AlarmData();
+                            ad.Code = worksheet.Cells["A" + i.ToString()].Value == null ? "Null" : worksheet.Cells["A" + i.ToString()].Value.ToString();
+                            ad.Content = worksheet.Cells["B" + i.ToString()].Value == null ? "Null" : worksheet.Cells["B" + i.ToString()].Value.ToString();
+                            ad.Type = worksheet.Cells["C" + i.ToString()].Value == null ? "Null" : worksheet.Cells["C" + i.ToString()].Value.ToString();
+                            ad.Start = DateTime.Now;
+                            ad.End = DateTime.Now;
+                            ad.State = false;
+                            AlarmList.Add(ad);
+                        }
+                        AddMessage("读取到" + worksheet.Dimension.End.Row.ToString() + "条报警");
+                    }
+                }
+                else
+                {
+                    AddMessage("X1621串线下料机报警.xlsx 文件不存在");
+                }
+            }
+            catch (Exception ex)
+            {
+                AddMessage(ex.Message);
+            }
+            #endregion
+
+
             string ip = Inifile.INIGetStringValue(iniParameterPath, "FX5U", "Ip", "192.168.0.20");
             int port = int.Parse(Inifile.INIGetStringValue(iniParameterPath, "FX5U", "Port", "504"));
             Fx5u = new Fx5u(ip, port);
@@ -62,14 +122,32 @@ namespace TUCHX1621UNLOADUI
             ScanB = new Scan();
             COM = Inifile.INIGetStringValue(iniParameterPath, "Scan", "ScanB", "COM3");
             ScanB.ini(COM);
-            UpdateUI();
+            UpdateUI();            
             Task.Run(() => { Run(); });
+            BigDataRun();
+            #region 更新本地时间
+            try
+            {
+                SXJLibrary.Oracle oraDB = new SXJLibrary.Oracle("qddb04.eavarytech.com", "mesdb04", "ictdata", "ictdata*168");
+                if (oraDB.isConnect())
+                {
+                    string oracleTime = oraDB.OraclDateTime();
+                    AddMessage("更新数据库时间到本地" + oracleTime);
+                }
+                oraDB.disconnect();
+            }
+            catch (Exception ex)
+            {
+                AddMessage(ex.Message);
+            }
+            #endregion
         }
         async void UpdateUI()
         {
             while (true)
             {
                 await Task.Delay(200);
+                #region 更新界面
                 if (Fx5u.Connect)
                 {
                     EllipsePLCState.Fill = Brushes.Green;
@@ -87,7 +165,59 @@ namespace TUCHX1621UNLOADUI
                     EllipsePLCState2.Fill = Brushes.Red;
                 }
                 CycleText.Text = SWms.ToString() + " ms";
-            }            
+                #endregion
+                #region 换班
+                if (LastBanci != GetBanci())
+                {
+                    LastBanci = GetBanci();
+                    Inifile.INIWriteValue(iniParameterPath, "Summary", "LastBanci", LastBanci);
+                    LampGreenElapse = 0;
+                    Inifile.INIWriteValue(iniParameterPath, "BigData", "LampGreenElapse", LampGreenElapse.ToString());
+                    LampGreenFlickerElapse = 0;
+                    Inifile.INIWriteValue(iniParameterPath, "BigData", "LampGreenFlickerElapse", LampGreenFlickerElapse.ToString());
+                    LampYellowElapse = 0;
+                    Inifile.INIWriteValue(iniParameterPath, "BigData", "LampYellowElapse", LampYellowElapse.ToString());
+                    LampYellowFlickerElapse = 0;
+                    Inifile.INIWriteValue(iniParameterPath, "BigData", "LampYellowFlickerElapse", LampYellowFlickerElapse.ToString());
+                    LampRedElapse = 0;
+                    Inifile.INIWriteValue(iniParameterPath, "BigData", "LampRedElapse", LampRedElapse.ToString());
+                    await Task.Run(() =>
+                    {
+                        Mysql mysql = new Mysql();
+                        try
+                        {
+                            int _result = -999;
+                            if (mysql.Connect())
+                            {
+                                string stm = string.Format("INSERT INTO HA_F4_LIGHT (PM,LIGHT_ID,MACID,CLASS,LIGHT,SDATE,STIME,ALARM,TIME_1,TIME_2,TIME_3,TIME_4,TIME_5,GROUP1,TRACK) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','0','0','0','0','0','{8}','{9}')"
+                                    , _PM, _LIGHT_ID, _MACID, GetBanci(), LampColor.ToString(), DateTime.Now.ToString("yyyyMMdd"), DateTime.Now.ToString("HHmmss"), "NA", GROUP1, TRACK);
+                                _result = mysql.executeQuery(stm);
+                            }
+                            this.Dispatcher.Invoke(new Action(() =>
+                            {
+                                AddMessage("插入数据库灯信号" + _result.ToString());
+                            }));
+                            
+                        }
+                        catch (Exception ex)
+                        {
+                            this.Dispatcher.Invoke(new Action(() =>
+                            {
+                                AddMessage(ex.Message);
+                            }));
+                            
+                        }
+                        finally
+                        {
+                            mysql.DisConnect();
+                        }
+                    });
+
+                    AddMessage(LastBanci + " 换班数据清零");
+                }
+                #endregion
+
+            }
         }
         void Run()
         {
@@ -126,11 +256,202 @@ namespace TUCHX1621UNLOADUI
                         ScanB.GetBarCode(ScanBGetBarcodeCallback);
                     }
                     #endregion
+                    #region 大数据读取
+                    //读报警
+                    M300 = Fx5u_2.ReadMultiM("M300", 64);
+                    //读三色灯状态
+                    LampColor = Fx5u_2.ReadW("D200");
+                    #endregion
                 }
                 catch
                 { }
                 SWms = sw.ElapsedMilliseconds;
             }
+        }
+        async void BigDataRun()
+        {
+            int _LampColor = LampColor;
+            int count1 = 0;
+            LampGreenSw.Start();
+            while (true)
+            {
+                await Task.Delay(1000);//每秒刷新               
+                #region 报警
+                if (M300 != null && Fx5u_2.Connect)
+                {
+                    for (int i = 0; i < AlarmList.Count; i++)
+                    {
+                        if (M300[i] != AlarmList[i].State && AlarmList[i].Content != "Null" && LampGreenSw.Elapsed.TotalMinutes > 3)
+                        {
+                            AlarmList[i].State = M300[i];
+                            if (AlarmList[i].State)
+                            {
+                                CurrentAlarm = AlarmList[i].Content;
+
+                                AlarmList[i].Start = DateTime.Now;
+                                AlarmList[i].End = DateTime.Now;
+                                AddMessage(AlarmList[i].Code + AlarmList[i].Content + "发生");
+
+                                string result = await Task<string>.Run(() =>
+                                {
+                                    try
+                                    {
+                                        int _result = -999;
+                                        Mysql mysql = new Mysql();
+                                        if (mysql.Connect())
+                                        {
+                                            string stm = string.Format("INSERT INTO HA_F4_DATA_ALARM (PM, GROUP1,TRACK,MACID,NAME,SSTARTDATE,SSTARTTIME,SSTOPDATE,SSTOPTIME,TIME,CLASS) VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}')"
+                                                , _PM, _GROUP1, _TRACK, _MACID, AlarmList[i].Content, AlarmList[i].Start.ToString("yyyyMMdd"), AlarmList[i].Start.ToString("HHmmss"), AlarmList[i].End.ToString("yyyyMMdd"), AlarmList[i].End.ToString("hhmmss"), "0", GetBanci());
+                                            _result = mysql.executeQuery(stm);
+                                        }
+                                        mysql.DisConnect();
+                                        return _result.ToString();
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        return ex.Message;
+                                    }
+                                });
+                                AddMessage("插入报警" + result);
+
+                                AlarmAction(i);//等待报警结束
+                            }
+
+                        }
+                    }
+
+                }
+                #endregion
+                #region 灯号更新
+                switch (LampColor)
+                {
+                    case 1:
+                        LampGreenElapse += 1;
+                        Inifile.INIWriteValue(iniParameterPath, "BigData", "LampGreenElapse", LampGreenElapse.ToString());
+                        break;
+                    case 2:
+                        LampGreenFlickerElapse += 1;
+                        Inifile.INIWriteValue(iniParameterPath, "BigData", "LampGreenFlickerElapse", LampGreenFlickerElapse.ToString());
+                        break;
+                    case 3:
+                        LampYellowElapse += 1;
+                        Inifile.INIWriteValue(iniParameterPath, "BigData", "LampYellowElapse", LampYellowElapse.ToString());
+                        break;
+                    case 4:
+                        LampYellowFlickerElapse += 1;
+                        Inifile.INIWriteValue(iniParameterPath, "BigData", "LampYellowFlickerElapse", LampYellowFlickerElapse.ToString());
+                        break;
+                    case 5:
+                        LampRedElapse += 1;
+                        Inifile.INIWriteValue(iniParameterPath, "BigData", "LampRedElapse", LampRedElapse.ToString());
+                        break;
+                    default:
+                        break;
+                }
+                count1++;
+                if (_LampColor != LampColor || count1 > 60)
+                {
+
+                    if (LampColor == 1 && _LampColor != LampColor)
+                    {
+                        LampGreenSw.Restart();
+                    }
+                    _LampColor = LampColor;
+                    count1 = 0;
+                    string result = await Task<string>.Run(() =>
+                    {
+                        try
+                        {
+                            int _result = -999;
+                            Mysql mysql = new Mysql();
+                            if (mysql.Connect())
+                            {
+                                string currentAlarm = LampColor == 4 ? CurrentAlarm : "NA";
+                                string stm = string.Format("UPDATE HA_F4_LIGHT SET LIGHT = '{3}',SDATE = '{4}',STIME = '{5}',ALARM = '{6}',TIME_1 = '{8}',TIME_2 = '{9}',TIME_3 = '{10}',TIME_4 = '{11}',TIME_5 = '{12}' WHERE PM = '{0}' AND LIGHT_ID = '{1}' AND MACID = '{2}' AND CLASS = '{7}'"
+                                    , _PM, _LIGHT_ID, _MACID, LampColor.ToString(), DateTime.Now.ToString("yyyyMMdd"), DateTime.Now.ToString("HHmmss"), currentAlarm, GetBanci(), ((double)LampGreenElapse / 60).ToString("F2"), ((double)LampGreenFlickerElapse / 60).ToString("F2"), ((double)LampYellowElapse / 60).ToString("F2")
+                                    , ((double)LampYellowFlickerElapse / 60).ToString("F2"), ((double)LampRedElapse / 60).ToString("F2"));
+                                _result = mysql.executeQuery(stm);
+                            }
+                            mysql.DisConnect();
+                            return _result.ToString();
+                        }
+                        catch (Exception ex)
+                        {
+                            return ex.Message;
+                        }
+                    });
+                    //AddMessage("更新灯信号" + result);
+                }
+                if (LampColor != 1)
+                {
+                    LampGreenSw.Reset();
+                }
+                #endregion
+                #region 机台指标
+
+                #endregion
+            }
+
+        }
+        async void AlarmAction(int i)
+        {
+            while (M300[i])
+            {
+                await Task.Delay(100);
+            }
+            AlarmList[i].End = DateTime.Now;
+            AddMessage(AlarmList[i].Code + AlarmList[i].Content + "解除");
+            TimeSpan time = AlarmList[i].End - AlarmList[i].Start;
+            await Task.Run(() =>
+            {
+                Mysql mysql = new Mysql();
+                try
+                {
+                    int _result = -999;
+                    if (mysql.Connect())
+                    {
+                        string stm = string.Format("UPDATE HA_F4_DATA_ALARM SET SSTOPDATE = '{5}',SSTOPTIME = '{6}',TIME = '{7}' WHERE PM = '{0}' AND MACID = '{1}' AND NAME = '{2}' AND SSTARTDATE = '{3}' AND SSTARTTIME = '{4}'"
+                            , _PM, _MACID, AlarmList[i].Content, AlarmList[i].Start.ToString("yyyyMMdd"), AlarmList[i].Start.ToString("HHmmss"), AlarmList[i].End.ToString("yyyyMMdd"), AlarmList[i].End.ToString("HHmmss"), time.TotalMinutes.ToString("F2"));
+                        _result = mysql.executeQuery(stm);
+                    }
+                    this.Dispatcher.Invoke(new Action(() =>
+                    {
+                        AddMessage("更新报警到数据库" + _result);
+                    }));
+                    
+                }
+                catch (Exception ex)
+                {
+                    this.Dispatcher.Invoke(new Action(() =>
+                    {
+                        AddMessage(ex.Message);
+                    }));
+                }
+                finally
+                {
+                    mysql.DisConnect();
+                }
+            });
+        }
+        private string GetBanci()
+        {
+            string rs = "";
+            if (DateTime.Now.Hour >= 8 && DateTime.Now.Hour < 20)
+            {
+                rs += DateTime.Now.ToString("yyyyMMdd") + "D";
+            }
+            else
+            {
+                if (DateTime.Now.Hour >= 0 && DateTime.Now.Hour < 8)
+                {
+                    rs += DateTime.Now.AddDays(-1).ToString("yyyyMMdd") + "N";
+                }
+                else
+                {
+                    rs += DateTime.Now.ToString("yyyyMMdd") + "D";
+                }
+            }
+            return rs;
         }
         void ScanAGetBarcodeCallback(string barcode)
         {
@@ -419,6 +740,21 @@ namespace TUCHX1621UNLOADUI
         private void FreeBordBarcodeButtonClick(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void BigDataParamClicked(object sender, RoutedEventArgs e)
+        {
+            _PM = PM.Text;
+            _GROUP1 = GROUP1.Text;
+            _TRACK = TRACK.Text;
+            _MACID = MACID.Text;
+            _LIGHT_ID = LIGHT_ID.Text;
+            Inifile.INIWriteValue(iniParameterPath, "BigData", "PM", PM.Text);
+            Inifile.INIWriteValue(iniParameterPath, "BigData", "GROUP1", GROUP1.Text);
+            Inifile.INIWriteValue(iniParameterPath, "BigData", "TRACK", TRACK.Text);
+            Inifile.INIWriteValue(iniParameterPath, "BigData", "MACID", MACID.Text);
+            Inifile.INIWriteValue(iniParameterPath, "BigData", "LIGHT_ID", LIGHT_ID.Text);
+            AddMessage("参数保存完成");
         }
 
         private void 扫码AClick(object sender, RoutedEventArgs e)
